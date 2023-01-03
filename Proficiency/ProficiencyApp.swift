6c62ce2ccb0746625d589edfc75ea2117e1ff129
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2023-01-02 19:40:50 -0500

    Initial Commit

---
+++ Proficiency/ProficiencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProficiencyApp.swift
+//  Proficiency
+//
+//  Created by Gerard Gomez on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ProficiencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
